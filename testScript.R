00b14afe530307428f3ca9cf4499b9f291de3429
Ismael Shaban <ismael.shaban.pina@gmail.com>
2019-01-03 20:43:10 +0100

    testScriptUpdated

--- testScript.R
+++ testScript.R
@@ -9,3 +9,13 @@ print(paste("This is a hands on project written in",
             subject,
             "subject",
             sep = " "))
+
+
+library("ggplot2")
+url <- "https://raw.githubusercontent.com/offensive-security/exploitdb/master/files_exploits.csv"
+url2 <- "https://raw.githubusercontent.com/offensive-security/exploit-database/master/files.csv"
+exploitdb <-url
+download.file(exploitdb, destfile = "db")
+db <- read.csv("./db", header = T)
+#db_aggr <- dplyr::count(db, platform, sort = T)
+#ggplot(db_aggr, aes(x="", y=n, fill=platform))+ geom_bar(width = 1, stat = "identity")
